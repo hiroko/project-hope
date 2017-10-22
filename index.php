0429d8821b3d3904ab55f647b8d5dc147d1564c6
Hiro <xhirokox@gmail.com>
2017-10-22 15:44:46 -0400

    Day 41

--- index.php
+++ index.php
@@ -27,24 +27,50 @@
   <body>
 <div class="container-fluid">
   <div class="row justify-content-center">
-    <div class="col-sm-8" id="content">
+    <div class="col-sm-6" id="content">
 
       <div class="container-fluid" id="top">
         <div class="row justify-content-center">
           <div class="col-sm-8 text-center">
 
-          <h1 style="font-family: satisfy; font-size: 90px; padding: 15px;">Project Hope</h1>
-            <p><i>Project Hope</i> brings you real voices from survivors, family members, and other loved ones.</p>
-            <p>Find us at <a href="http://burgundyforlife.org" target="_blank">Burgundy for Life</a>.</p>
+          <h1 style="font-family: satisfy; font-size: 85px; padding: 15px;">Project Hope</h1>
+            <p>Feeling alone after a diagnosis of vascular malformation, stroke, brain injury, or treatment?<br>
+            <em>Project Hope</em> publishes real advice from those who have stepped through the same paths of uncertainly you are walking.</p>
+            <p><em>Project Hope</em> curates voices from survivors and family members who took time to fill out a survey by <a href="http://burgundyforlife.org" target="_blank">Burgundy for Life</a> for #AVMAwarenessMonth in October, 2017.</p>
+            <p>Refresh the page to "rotate" through the advices fellow survivors and their loved ones have provided.</p>
 
           </div>
         </div>
 
         <div class="row justify-content-center">
-          <div class="col-sm-5 text-center">
-            <?php text_randomizer(); ?>
+          <div class="col-sm-6 text-center">
+            <blockquote style="background-color: #f2d7de; padding: 20px; margin: 10px; outline: 2px solid #f2d7de; outline-offset: 15px; outline-radius: 15px; border-radius: 15px;">
+              <h2 style="font-family: Raleway; font-size: 25px; padding: 15px;">
+                <?php text_randomizer(); ?>
+              </h2>
+            </blockquote>
+            <p style="padding: 10px;"><small><i class="fa fa-angle-double-right fa-fw"></i><a href="index.php">Next</a></small></p>
           </div>
         </div>
+
+        <!-- SNS -->
+            <section id="SNS">
+              <div class="container-fluid"><!-- container-fluid -->
+                <div class="row justify-content-center"><!-- row -->
+                  <div class="col-sm-3 text-center"><!-- col -->
+                    <i class="fa fa-facebook-official fa-lg fa-fw"></i>&nbsp; <a href="http://facebook.com/avmalformation" target="_blank">/avmalformation</a>
+                  </div><!-- /col -->
+                  <div class="col-sm-3 text-center"><!-- col -->
+                    <i class="fa fa-twitter fa-lg fa-fw"></i>&nbsp; <a href="http://twitter.com/avmalformation" target="_blank">@avmalformation</a>
+                  </div><!-- /col -->
+
+                  <div class="col-sm-3 text-center"><!-- col -->
+                    <i class="fa fa-envelope fa-lg fa-fw"></i>&nbsp; <a href="mailto:hi@burgundyforlife.org">hi@burgundyforlife.org</a>
+                  </div><!-- /col -->
+                </div><!-- /row -->
+              </div><!-- /container-fluid -->
+          </section>
+          <!-- /SNS -->
 
       <div class="row justify-content-center" id="footer">
         <div class="col-sm-12 text-center">
